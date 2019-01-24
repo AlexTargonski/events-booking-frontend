16b5268c5d9f5ea7619d82e8ea834b38c227b974
Oleksandr <targonsky97@gmail.com>
2019-01-24 18:07:03 +0200

    feat: add links to navbar

--- src/modules/layouts/NavBar.js
+++ src/modules/layouts/NavBar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import styled               from 'styled-components';
+import { Link }             from 'react-router-dom';
 
 class LoginPage extends Component {
   render() {
     return (
       <Wrapper>
-        Navbar
+        <NavBarLink to="/login">
+          Login
+        </NavBarLink>
+        <span style={{color : '#ffff'}}>
+          |
+        </span>
+        <NavBarLink to="/">
+          Sign Up
+        </NavBarLink>
       </Wrapper>
     )
   }
@@ -14,6 +23,12 @@ class LoginPage extends Component {
 const Wrapper = styled.div`
   background : #222534;
   padding    : 1% 5%;
+`;
+
+const NavBarLink = styled(Link)`
+  color           : #ffff;
+  padding         : 0 5px;
+  text-decoration : none;
 `;
 
 export default LoginPage;
