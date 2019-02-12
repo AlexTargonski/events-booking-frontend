7e3445a76dd8acf7b2540b8c0945e300a8130c73
Oleksandr <targonsky97@gmail.com>
2019-02-12 18:18:41 +0200

    feat: use modal in Events page

--- src/modules/events/pages/EventsPage.js
+++ src/modules/events/pages/EventsPage.js
@@ -1,16 +1,46 @@
-import React  from 'react';
+import React, { Component } from 'react';
 
-const EventsPage = () => (
-  <div>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type
-    pecimen book. It has survived not only five centuries, but also the leap into
-   electronic typesetting, remaining essentially unchanged. It was popularised
-   in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-   and more recently with desktop publishing software like Aldus
-   PageMaker including versions of Lorem Ipsum.
-  </div>
-)
+import Modal                from '../../layouts/Modal';
+
+class EventsPage extends Component {
+  state = {
+    creating : false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div>
+          <button onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default EventsPage;
