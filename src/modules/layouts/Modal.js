27fadf18b8e939b002a95f43f30367ca659f93b9
Oleksandr <targonsky97@gmail.com>
2019-02-12 18:18:04 +0200

    feat: add modal

---
+++ src/modules/layouts/Modal.js
@@ -0,0 +1,52 @@
+import React  from 'react';
+import styled from 'styled-components';
+
+const Modal = props => (
+  <ModalWrapper>
+    <Header>
+      <h1>{props.title}</h1>
+    </Header>
+    <Content>
+      {props.children}
+    </Content>
+    <Actions>
+      {props.canCancel && (
+        <button onClick={props.onCancel}>
+          Cancel
+        </button>
+      )}
+      {props.canConfirm && (
+        <button onClick={props.onConfirm}>
+          Confirm
+        </button>
+      )}
+    </Actions>
+  </ModalWrapper>
+);
+
+const ModalWrapper = styled.div`
+  width      : 90%;
+  background : white;
+  box-shadow : 0 2px 8px rgba(0, 0, 0, 0.26);
+  position   : fixed;
+  top        : 20vh;
+  left       : 5%;
+`;
+
+const Header = styled.div`
+  padding    : 12px;
+  background : #222534;
+  color      : white;
+`;
+
+const Content = styled.div`
+  padding : 10px;
+`;
+
+const Actions = styled.div`
+  display         : flex;
+  justify-content : flex-end;
+  padding         : 1rem;
+`;
+
+export default Modal;
