8483ab05b52e0fd61099fb6bb77343c5558f13f0
Oleksandr <targonsky97@gmail.com>
2019-02-15 16:39:49 +0200

    fix: remove button styles

--- src/modules/events/forms/CreateEvent.js
+++ src/modules/events/forms/CreateEvent.js
@@ -62,21 +62,4 @@ const FormInput = styled.input`
   padding : 16px;
 `;
 
-const FormButton = styled.button`
-  && {
-    background : #36c2b9;
-    color      : #ffff;
-    border     : none;
-    width      : 245px;
-    height     : 60px;
-    margin     : 1% auto;
-    cursor     : pointer;
-    font-size  : 18px;
-   }
-
-   &:hover {
-     background : #4bb8b1;
-   }
-`;
-
 export default CreateEvent;
