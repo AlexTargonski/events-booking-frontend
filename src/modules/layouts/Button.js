6020be5274710c3b3494e8f9699bdf8de9ec59da
Oleksandr <targonsky97@gmail.com>
2019-03-04 17:18:37 +0200

    style: change button size

--- src/modules/layouts/Button.js
+++ src/modules/layouts/Button.js
@@ -5,8 +5,8 @@ export default styled.button`
     background : #36c2b9;
     color      : #ffff;
     border     : none;
-    width      : 245px;
-    height     : 60px;
+    width      : 185px;
+    height     : 45px;
     margin     : 1% auto;
     cursor     : pointer;
     font-size  : 18px;
