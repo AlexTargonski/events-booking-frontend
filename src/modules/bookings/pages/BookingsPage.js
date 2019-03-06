000ac1ba4c61e585e8acc6bf8b45aecd97f5e8c0
Oleksandr <targonsky97@gmail.com>
2019-03-06 15:56:09 +0200

    feat: fetch bookings

--- src/modules/bookings/pages/BookingsPage.js
+++ src/modules/bookings/pages/BookingsPage.js
@@ -1,16 +1,81 @@
-import React  from 'react';
+import React, { Component } from 'react';
+import moment               from 'moment';
 
-const BookingsPage = () => (
-  <div>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it to make a type
-    pecimen book. It has survived not only five centuries, but also the leap into
-   electronic typesetting, remaining essentially unchanged. It was popularised
-   in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-   and more recently with desktop publishing software like Aldus
-   PageMaker including versions of Lorem Ipsum.
-  </div>
-)
+import AuthContext          from '../../../context/auth-context';
+
+class BookingsPage extends Component {
+  state = {
+    loaded    : false,
+    bookings  : [],
+  };
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    const requestBody = {
+      query: `
+        query {
+          bookings {
+            _id
+           createdAt
+           event {
+             _id
+             title
+             date
+           }
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8080/graphiql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+    .then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings: bookings, loaded: false });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { bookings, loaded } = this.state
+
+    return (
+      <React.Fragment>
+        {
+          loaded?
+          <p>loading</p>
+          :
+          <ul>
+            {bookings.map(booking => (
+              <li key={booking._id}>
+                {booking.event.title} -{' '}
+                {moment(booking.createdAt.date).format('D MMM HH:mm')}
+              </li>
+            ))}
+          </ul>
+        }
+      </React.Fragment>
+    );
+  }
+}
 
 export default BookingsPage;
