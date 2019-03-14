6e61033b123717a56d427c713678763e4cfb6994
Oleksandr <targonsky97@gmail.com>
2019-03-14 15:47:06 +0200

    feat: delete bookings

--- src/modules/bookings/pages/BookingsPage.js
+++ src/modules/bookings/pages/BookingsPage.js
@@ -56,6 +56,45 @@ class BookingsPage extends Component {
       });
   };
 
+  deleteBooking = bookingId => {
+    const requestBody = {
+      query: `
+          mutation {
+            cancelBooking(bookingId: "${bookingId}") {
+            _id
+             title
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8080/graphiql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        this.setState(prevState => {
+          const updatedBookings = prevState.bookings.filter(booking => {
+            return booking._id !== bookingId;
+          });
+          return { bookings: updatedBookings, loaded: false };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     const { bookings, loaded } = this.state
 
@@ -70,6 +109,9 @@ class BookingsPage extends Component {
               <Booking key={booking._id}>
                 {booking.event.title} -{' '}
                 {moment(booking.createdAt.date).format('D MMM HH:mm')}
+                <button onClick={this.deleteBooking.bind(this, booking._id)}>
+                  Cancel
+                </button>
               </Booking>
             ))}
           </ul>
